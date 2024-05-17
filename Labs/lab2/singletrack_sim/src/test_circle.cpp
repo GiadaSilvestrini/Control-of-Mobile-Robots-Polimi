ec3b8b8d41920d2caf2e2f2d2643c18b1e114551
GiadaSilvestrini <100673995+GiadaSilvestrini@users.noreply.github.com>
2024-05-17 08:36:50 +0200

    G

---
+++ Labs/lab2/singletrack_sim/src/test_circle.cpp
@@ -0,0 +1,66 @@
+#include "singletrack_sim/test_circle.h"
+
+void test_circle::Prepare(void)
+{
+    /* ROS topics */
+    vehicleCommand_publisher = Handle.advertise<std_msgs::Float64MultiArray>("/car_input", 1);
+
+    /* Initialize node state */
+    RunPeriod = RUN_PERIOD_DEFAULT;
+
+    steer = speed = 0.0;
+
+    ROS_INFO("Node %s ready to run.", ros::this_node::getName().c_str());
+}
+
+void test_circle::RunPeriodically(float Period)
+{
+    ros::Rate LoopRate(1.0/Period);
+
+    ROS_INFO("Node %s running periodically (T=%.2fs, f=%.2fHz).", ros::this_node::getName().c_str(), Period, 1.0/Period);
+
+    while (ros::ok())
+    {
+        PeriodicTask();
+
+        ros::spinOnce();
+
+        LoopRate.sleep();
+    }
+}
+
+void test_circle::Shutdown(void)
+{
+    ROS_INFO("Node %s shutting down.", ros::this_node::getName().c_str());
+}
+
+void test_circle::PeriodicTask(void)
+{
+    /* Circle trajectory generation */
+    // Trajectory parameters (these parameters should be moved to the parameter server)
+    const double R = 1.0;
+    const double w = 2.0;
+
+    // Trajectory computation
+    xref    = R*(std::cos(w*ros::Time::now().toSec())-1.0);
+    dxref   = -w*R*std::sin(w*ros::Time::now().toSec());
+    ddxref  = -std::pow(w,2.0)*R*std::cos(w*ros::Time::now().toSec());
+    yref    = R*std::sin(w*ros::Time::now().toSec());
+    dyref   = w*R*std::cos(w*ros::Time::now().toSec());
+    ddyref  = -std::pow(w,2.0)*R*std::sin(w*ros::Time::now().toSec());
+
+    /* Vehicle commands */
+    // Vehicle parameters (these parameters should be moved to the parameter server)
+    const double L = 0.26;
+
+    // Flatness transformation
+    speed = std::sqrt(std::pow(dxref,2.0)+std::pow(dyref,2.0));
+    steer = std::atan((dxref*ddyref-dyref*ddxref)/std::pow(std::pow(dxref,2.0)+std::pow(dyref,2.0),1.5)*L);
+
+    /* Publishing vehicle commands (t, msg->data[0]; velocity, msg->data[1]; steer, msg->data[2]) */
+    std_msgs::Float64MultiArray msg;
+    msg.data.push_back(ros::Time::now().toSec());
+    msg.data.push_back(speed);
+    msg.data.push_back(steer);
+    vehicleCommand_publisher.publish(msg);
+}
---
+++ Labs/lab2/singletrack_sim/src/test_circle_node.cpp
@@ -0,0 +1,18 @@
+#include "singletrack_sim/test_circle.h"
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, NAME_OF_THIS_NODE);
+  
+  test_circle test_circle_node;
+   
+  test_circle_node.Prepare();
+  
+  test_circle_node.RunPeriodically(test_circle_node.RunPeriod);
+  
+  test_circle_node.Shutdown();
+  
+  return (0);
+}
+
